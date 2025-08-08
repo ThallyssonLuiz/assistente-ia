7206299016ea5f55c9dcdea6b67ef2f5d0b4461f
Blurry Footprint <200711513+Blurry-Footprint@users.noreply.github.com>
2025-08-08 01:11:06 -0300

    Implementa persistência do tema usando Local Storage

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const toggleBtn = document.getElementById("themeToggle");
 function toggleTheme() {
   body.classList.toggle("dark-mode");
   updateIcon();
+  saveThemePreference();
 }
 
 function updateIcon() {
@@ -14,4 +15,24 @@ function updateIcon() {
   }
 }
 
-updateIcon();
+function saveThemePreference() {
+  if (body.classList.contains("dark-mode")) {
+    localStorage.setItem("theme", "dark");
+  } else {
+    localStorage.setItem("theme", "light");
+  }
+}
+
+function loadThemePreference() {
+  const savedTheme = localStorage.getItem("theme");
+  if (savedTheme === "dark") {
+    body.classList.add("dark-mode");
+  } else {
+    body.classList.remove("dark-mode");
+  }
+  updateIcon();
+}
+
+window.onload = () => {
+  loadThemePreference();            
+};
